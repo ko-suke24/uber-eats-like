7adbfd64bf4db8a58c3ffdad6358de682254e857
ko-suke24 <kosuke0322@gmail.com>
2021-02-10 09:41:28 +0900

    フロントエンド処理11

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,6 +1,37 @@
-import React, { Fragment } from 'react';
+// --- 次でuseReducerを追加 ---
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+import { fetchLineFoods } from '../apis/line_foods';
+// --- ここから追加 ---
+// reducers
+import {
+  initialState,
+  lineFoodsActionTyps,
+  lineFoodsReducer,
+} from '../reducers/lineFoods';
+// --- ここまで追加 ---
 
 export const Orders = () => {
+  // --- ここから追加 ---
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+  // --- ここまで追加 ---
+
+  useEffect(() => {
+    // --- ここから修正 ---
+    dispatch({ type: lineFoodsActionTyps.FETCHING });
+    fetchLineFoods()
+      .then((data) =>
+        dispatch({
+          type: lineFoodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data
+          }
+        })
+      )
+      .catch((e) => console.error(e));
+    // --- ここから修正 ---
+  }, []);
+  
   return (
     <Fragment>
       注文画面
