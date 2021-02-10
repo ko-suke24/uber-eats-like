99fe2cde6c100e029dfbb75cab8adb829780aafc
ko-suke24 <kosuke0322@gmail.com>
2021-02-10 10:49:11 +0900

    フロントエンド処理12

--- frontend/src/apis/line_foods.js
+++ frontend/src/apis/line_foods.js
@@ -1,43 +1,36 @@
-// --- ここから追加 ---
-import { REQUEST_STATE } from '../constants';
+import axios from 'axios';
+import { lineFoods, lineFoodsReplace } from '../urls/index'
 
-export const initialState = {
-  fetchState: REQUEST_STATE.INITIAL, // 取得状況
-  postState: REQUEST_STATE.INITIAL,  // 登録状況
-  lineFoodsSummary: null,            // 仮注文データ
+export const postLineFoods = async(params) => {
+  return await axios.post(lineFoods,
+    {
+      food_id: params.foodId,
+      count: params.count,
+    }
+  )
+  .then(res => {
+    return res.data
+  })
+  .catch((e) => { throw e; })
 };
 
-export const lineFoodsActionTyps = {
-  FETCHING: 'FETCHING',
-  FETCH_SUCCESS: 'FETCH_SUCCESS',
-  POSTING: 'POSTING',
-  POST_SUCCESS: 'POST_SUCCESS',
-}
+export const replaceLineFoods = async(params) => {
+  return await axios.put(lineFoodsReplace,
+    {
+      food_id: params.foodId,
+      count: params.count,
+    }
+  )
+  .then(res => {
+    return res.data
+  })
+  .catch((e) => { throw e; })
+};
 
-export const lineFoodsReducer = (state, action) => {
-  switch (action.type) {
-    case lineFoodsActionTyps.FETCHING:
-      return {
-        ...state,
-        fetchState: REQUEST_STATE.LOADING,
-      };
-    case lineFoodsActionTyps.FETCH_SUCCESS:
-      return {
-        fetchState: REQUEST_STATE.OK,
-        lineFoodsSummary: action.payload.lineFoodsSummary,
-      };
-    case lineFoodsActionTyps.POSTING:
-      return {
-        ...state,
-        postState: REQUEST_STATE.LOADING,
-      };
-    case lineFoodsActionTyps.POST_SUCCESS:
-      return {
-        ...state,
-        postState: REQUEST_STATE.OK,
-      };
-    default:
-      throw new Error();
-  }
-}
-// --- ここまで追加 ---
+export const fetchLineFoods = async() => {
+  return await axios.get(lineFoods)
+  .then(res => {
+    return res.data
+  })
+  .catch((e) => { throw e; })
+};
